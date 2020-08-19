ce1d11c67ce22f73c9f1c66796fb037f0d09178f
SilverLongjohns <jakehollinger@hotmail.co.uk>
2020-08-19 16:35:30 +0100

    placeholder background and css reorganising

--- src/clicker-game.js
+++ src/clicker-game.js
@@ -1,6 +1,8 @@
 class Esme {
   constructor() {
     this.craftingMaterials = 0;
+    this.scytheEnabled = false;
+    this.scythes = 0;
     this.retainers = 0;
     this.airships = 0;
   };
@@ -11,14 +13,25 @@ class Esme {
     this.retainerCostString = document.getElementById("retainerCost")
     this.airshipString = document.getElementById("airships")
     this.airshipCostString = document.getElementById("airshipCost")
+    this.scytheCostString = document.getElementById("eScytheCost")
+    this.scytheButton = document.getElementById("scytheButton")
   };
-
-  
 
   harvestPlants(num) {
     this.craftingMaterials += num;
     this.pointsString.innerHTML = this.craftingMaterials;
   };
+
+  enableScythe() {
+    var scytheCost = 50
+    if(this.craftingMaterials >= scytheCost) {
+      this.scythes = 1;
+      this.craftingMaterials -= scytheCost;
+      this.scytheCostString.innerHTML = "";
+      this.scytheButton.outerHTML = '<button id="scytheButton">Bought!</button>'
+    }
+
+  }
 
   hireRetainer() {
     var retainerCost = Math.floor(10 * Math.pow(1.1, this.retainers));
@@ -27,6 +40,8 @@ class Esme {
       this.craftingMaterials -= retainerCost;
       this.retainerCostString.innerHTML = retainerCost;
       this.retainerString.innerHTML = this.retainers;
+      var html = document.getElementById("retainerspace")
+      html.innerHTML += '<div id="retainerimg"></div>'
     };
     var nextRetainerCost = Math.floor(10 * Math.pow(1.1, this.retainers));
     this.retainerCostString.innerHTML = nextRetainerCost;
@@ -49,6 +64,7 @@ class Esme {
 const esme = new Esme();
 
 window.setInterval(function() {
+  esme.harvestPlants(esme.scythes)
   esme.harvestPlants(esme.retainers)
   esme.harvestPlants(esme.airships * 5)
 }, 1000)
